e1c4037e2a71f3906e4be8f260d5d244c006558b
Bkwak10101 <bartekkwak@gmail.com>
2023-08-25 21:59:29 +0200

    Initial commit

---
+++ src/main/java/com/github/bkwak/springparkingapp/SpringParkingAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.bkwak.springparkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParkingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringParkingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/bkwak/springparkingapp/SpringParkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bkwak.springparkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringParkingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
