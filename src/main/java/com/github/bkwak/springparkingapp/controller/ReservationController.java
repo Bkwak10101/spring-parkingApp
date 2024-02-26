b376d148a2a0476f5de53aa40358092ccb270106
Bkwak10101 <bartekkwak@gmail.com>
2024-02-26 20:24:34 +0100

    Updated reservation controller

--- src/main/java/com/github/bkwak/springparkingapp/controller/ReservationController.java
+++ src/main/java/com/github/bkwak/springparkingapp/controller/ReservationController.java
@@ -1,12 +1,17 @@
 package com.github.bkwak.springparkingapp.controller;
 
 import com.github.bkwak.springparkingapp.model.Reservation;
+import com.github.bkwak.springparkingapp.model.Spot;
 import com.github.bkwak.springparkingapp.model.User;
 import com.github.bkwak.springparkingapp.repository.ReservationRepository;
+import com.github.bkwak.springparkingapp.repository.SpotRepository;
 import com.github.bkwak.springparkingapp.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,17 +24,33 @@ public class ReservationController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private SpotRepository spotRepository;
+
     @CrossOrigin
     @PostMapping("/add")
-    public Reservation addReservation(@RequestBody Reservation reservation) {
-        Reservation newReservation = new Reservation();
-        newReservation.setStartDate(reservation.getStartDate());
-        newReservation.setEndDate(reservation.getEndDate());
+    public ResponseEntity<?> addReservation(@RequestBody Reservation reservation) {
         User user = userRepository.findByEmail("janush@gmail.com");
-        newReservation.setUser_id(user);
-        newReservation.setSpots(reservation.getSpots());
-        newReservation.setVehicle_id(reservation.getVehicle_id());
+        reservation.setUser_id(user);
+        Reservation newReservation = reservationRepository.save(reservation);
+
+        List<Spot> spots = newReservation.getSpots();
+        if (spots != null && !spots.isEmpty()) {
+            for (Spot spot : spots) {
+                updateSpotAvailability(spot.getSpot_id());
+            }
+        }
+
         log.info(newReservation.toString());
-        return reservationRepository.save(newReservation);
+        return ResponseEntity.ok(newReservation);
+    }
+    @PutMapping("/{spotId}")
+    public ResponseEntity<?> updateSpotAvailability(@PathVariable Long spotId) {
+        Spot spot = spotRepository.findById(spotId)
+                .orElseThrow(() -> new RuntimeException("Parking spot with the id doesn't exist: " + spotId));
+        spot.setAvailability(false);
+        spotRepository.save(spot);
+
+        return ResponseEntity.ok().build();
     }
 }
